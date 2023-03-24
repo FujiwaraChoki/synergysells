ceb323ffbd635afc790d398e0fc4e8385c4e7476
Sami Hindi <78088687+FujiwaraChoki@users.noreply.github.com>
2023-03-24 21:02:32 +0100

    Now using Link tag

--- pages/404.js
+++ pages/404.js
@@ -5,13 +5,13 @@ const NotFound = () => {
         <div class="bg-white py-6 sm:py-8 lg:py-12">
             <div class="mx-auto max-w-screen-2xl px-4 md:px-8">
                 <div class="flex flex-col items-center">
-                    <a href="/" class="text-black-800 mb-8 inline-flex items-center gap-2.5 text-2xl font-bold md:text-3xl" aria-label="logo">
+                    <Link href="/" class="text-black-800 mb-8 inline-flex items-center gap-2.5 text-2xl font-bold md:text-3xl" aria-label="logo">
                         <svg width="95" height="94" viewBox="0 0 95 94" class="h-auto w-6 text-indigo-500" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                             <path d="M96 0V47L48 94H0V47L48 0H96Z" />
                         </svg>
 
                         SynergySells
-                    </a>
+                    </Link>
 
                     <p class="mb-4 text-sm font-semibold uppercase text-indigo-500 md:text-base">Thas's a 404</p>
                     <h1 class="mb-2 text-center text-2xl font-bold text-gray-800 md:text-3xl">Page not found</h1>
