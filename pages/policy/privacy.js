8dd529033dce83ff8525fa04ea091e4a96224b9c
Sami Hindi <78088687+FujiwaraChoki@users.noreply.github.com>
2023-03-24 21:20:17 +0100

    s

---
+++ pages/policy/privacy.js
@@ -0,0 +1,67 @@
+import Link from "next/link"
+
+const Policy = () => {
+    return (
+        <div class="bg-white py-6 sm:py-8 lg:py-12">
+            <div class="mx-auto max-w-screen-md px-4 md:px-8">
+                <h1 class="mb-4 text-center text-2xl font-bold text-gray-800 sm:text-3xl md:mb-6">Privacy Policy for SynergySells</h1>
+                <p class="mb-6 text-gray-500 sm:text-lg md:mb-8">
+                    At SynergySells, we take your privacy seriously. This Privacy Policy explains how we collect, use, disclose, and safeguard your information when you visit our website and use our services. Please read this Privacy Policy carefully before using our website or services.<br />
+
+                    If you have any questions or concerns about our Privacy Policy, please contact us at the contact information provided at the end of this policy.<br />
+
+                    <h3 class="mb-2 text-lg font-bold text-gray-800 sm:text-xl">Collection of Information</h3>
+
+                    We may collect personal and non-personal information from you when you visit our website or use our services. Personal information includes your name, email address, phone number, and any other information that you provide to us. Non-personal information includes information about your device, browser, and usage of our website.<br />
+
+                    We collect this information to provide you with our services, improve our website and services, and communicate with you. We will not sell, share, or rent this information to any third parties without your consent, except as described in this policy.<br />
+
+                    <h3 class="mb-2 text-lg font-bold text-gray-800 sm:text-xl">Use of Information</h3>
+
+                    We may use the information we collect to:<br />
+
+                    * Provide and maintain our services<br />
+                    * Improve our website and services<br />
+                    * Respond to your inquiries and requests<br />
+                    * Send you promotional and marketing materials<br />
+                    * Monitor and analyze usage of our website and services<br />
+                    * Prevent fraudulent activity<br />
+                    * Comply with legal obligations<br />
+                    * Disclosure of Information<br />
+
+                    We may disclose your personal and non-personal information to third parties, including:<br />
+
+                    * Service providers who help us provide our services<br />
+                    * Law enforcement or government agencies as required by law<br />
+                    * Other users of our services as necessary to provide our services<br />
+                    * Our affiliates, partners, and successors as necessary to provide our services<br />
+                    * Other third parties with your consent<br />
+                    * Security of Information<br />
+
+                    We take reasonable measures to protect your personal and non-personal information from unauthorized access, use, or disclosure. However, no method of transmission over the internet or electronic storage is completely secure, and we cannot guarantee the absolute security of your information.<br />
+
+                    <h3 class="mb-2 text-lg font-bold text-gray-800 sm:text-xl">Links to other websites</h3>
+
+                    Our website may contain links to other websites that are not operated by us. We are not responsible for the privacy practices of these websites, and we recommend that you review their privacy policies before providing any personal information.<br />
+
+                    <h3 class="mb-2 text-lg font-bold text-gray-800 sm:text-xl">Changes to this pricacy policy</h3>
+
+                    We may update this Privacy Policy from time to time. Any changes will be posted on our website with an updated effective date. We encourage you to review this Privacy Policy periodically.<br />
+
+                    <h3 class="mb-2 text-lg font-bold text-gray-800 sm:text-xl">Contact us</h3>
+
+                    If you have any questions or concerns about our Privacy Policy, please contact us at:<br />
+
+                    SynergySells<br />
+                    sami@samihindi.com<br />
+
+                    Or through the <Link href="/contact">contact form </Link>on our website.
+
+                    Last updated: 24.03.2023
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Policy
